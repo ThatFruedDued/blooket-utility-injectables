13c59a29e9e26d03d2ae132bbe0a4f5825107c97
okr765 (ThatFruedDued) <okr765@gmail.com>
2021-12-29 11:20:21 -0500

    fix issue

--- injectables/test/cleanup.js
+++ injectables/test/cleanup.js
@@ -1,9 +1,1 @@
-delete blooketUtility.injectables[
-  blooketUtility.injectables.indexOf(
-    blooketUtility.injectables.filter(
-      (e) =>
-        e.url ===
-        "https://thatfrueddued.github.io/blooket-utility-injectables/injectables/test/injectable.json"
-    )[0]
-  )
-];
+delete blooketUtility.prefs.test;
