6369c9bf0141c660fd6fd4db32f3967c7a6b2b74
okr765 (ThatFruedDued) <okr765@gmail.com>
2021-12-29 16:09:27 -0500

    add customMaps injectable

---
+++ injectables/customMaps/setup.js
@@ -0,0 +1,20 @@
+if (!blooketUtility.prefs.customMaps) {
+  blooketUtility.prefs.customMaps = {
+    title: "Custom Maps",
+    description: "Custom tower defense maps",
+    value: {
+      islands: {
+        title: "Islands (Piotr)",
+        description:
+          "A custom map created by Piotr. Will load into the Sunny Meadows map slot.",
+        value: false,
+      },
+      volatileVolcano: {
+        title: "Volatile Volcano (Cheese)",
+        description:
+          "A custom map created by Cheese. Will load into the Abandoned Mine map slot.",
+        value: false,
+      },
+    },
+  };
+}
